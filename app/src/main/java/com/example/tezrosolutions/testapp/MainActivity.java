797416e3d8a33cc558edafd895663215fdaabb98
liquid11 <orangevolution@yahoo.com>
2015-08-20 13:34:07 -0700

    made custom widgets

--- app/src/main/java/com/example/tezrosolutions/testapp/MainActivity.java
+++ app/src/main/java/com/example/tezrosolutions/testapp/MainActivity.java
@@ -4,7 +4,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.util.Log;
+//import android.util.Log;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.graphics.Color;
+import android.widget.EditText;
+
 
 public class MainActivity extends ActionBarActivity {
     private static String TAG= "Message: " ;
@@ -12,15 +17,50 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // Log.i(TAG,"onCreate");
 
-        Log.i(TAG,"onCreate");
+        //Layout
+        RelativeLayout orelativeLayout = new RelativeLayout(this);
+        orelativeLayout.setBackgroundColor(Color.parseColor("#007293"));
+
+        //Button
+        Button oblueButton = new Button(this);
+        oblueButton.setId(2);
+        oblueButton.setText("Log In");
+        oblueButton.setBackgroundColor(Color.WHITE);
+
+        RelativeLayout.LayoutParams obuttonDetails= new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        //User Input
+        EditText ousername = new EditText(this);
+
+        ousername.setId(1);
+        obuttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        obuttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        RelativeLayout.LayoutParams ousernameDetails= new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        // wiget positions
+        ousernameDetails.addRule(orelativeLayout.ABOVE,oblueButton.getId());
+        ousernameDetails.addRule(orelativeLayout.CENTER_HORIZONTAL);
+        ousernameDetails.setMargins(0,0,0,50);
+ //todo: fix the user text width  left on tutorial 16
+        //adding to the layout
+        orelativeLayout.addView(ousername, ousernameDetails);
+        orelativeLayout.addView(oblueButton,obuttonDetails);
+        setContentView(orelativeLayout);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG, "onStart");
+        // Log.i(TAG, "onStart");
     }
 
     @Override
